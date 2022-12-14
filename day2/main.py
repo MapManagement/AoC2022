a11b46b66f54d18c828a15f75478d5b3c33ca3c8
Jan <MapManagement@protonmail.com>
2022-12-14 19:30:28 +0100

    Add second puzzle of day 2

--- day2/main.py
+++ day2/main.py
@@ -1,17 +1,53 @@
-def calculate_total_score():
+POINTS = {"AY": 8, "AX": 4, "AZ": 3, "BY": 5, "BX": 1, "BZ": 9, "CY": 2, "CX": 7, "CZ": 6}
+SIGN_POINTS = {"X": 1, "Y": 2, "Z": 3}
+PART_2_MOVES = {
+    "X": {
+        "A": "Z",
+        "B": "X",
+        "C": "Y"
+    },
+    "Y": {
+        "A": "X",
+        "B": "Y",
+        "C": "Z"
+    },
+    "Z": {
+        "A": "Y",
+        "B": "Z",
+        "C": "X"
+    }
+}
+
+def part1():
     with open("input", "r") as input:
         total_score = 0
         lines = input.readlines()
         # A = rock, B = paper, C = scissors
         # X1 = rock, Y2 = paper, Z3 = scissors
-        points = {"AY": 8, "AX": 4, "AZ": 3, "BY": 5, "BX": 1, "BZ": 9, "CY": 2, "CX": 7, "CZ": 6}
 
         for line in lines:
-            game_round = line.strip().replace(" ", "")
-            total_score += points[game_round]
+            game_moves = line.strip().replace(" ", "")
+            total_score += POINTS[game_moves]
 
         return total_score
 
+def part2():
+    with open("input", "r") as input:
+        total_score = 0
+        lines = input.readlines()
+
+        for line in lines:
+            signs = line.split(" ")
+            own_move = PART_2_MOVES[signs[1].strip()][signs[0]]
+            game_moves = signs[0] + own_move
+
+            total_score += POINTS[game_moves]
+
+        return total_score
+                    
+
 if __name__ == "__main__":
-    score = calculate_total_score()
-    print(score)
+    part1_score = part1()
+    part2_score = part2()
+    print(f"Part 1: {part1_score}")
+    print(f"Part 2: {part2_score}")
