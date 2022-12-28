2a116e6e5108a5b851f52f64a08d717438b0013c
Jan <MapManagement@protonmail.com>
2022-12-28 18:04:18 +0100

    Clean up day 9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
 
 fn main() {
-    let part1 = part1();
-    let part2 = part2();
+    let part1 = get_tail_positions(2);
+    let part2 = get_tail_positions(10);
 
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
@@ -44,56 +44,9 @@ fn read_file_lines() -> Vec<String> {
     lines
 }
 
-fn part1() -> usize {
+fn get_tail_positions(rope_length: usize) -> usize {
     let lines = read_file_lines();
-    let mut tail = Point { x: 0, y: 0 };
-    let mut head = Point { x: 0, y: 0 };
-    let mut all_tail_pos: Vec<Point> = Vec::new();
-
-    for line in lines {
-        let motion = parse_line(line);
-
-        for _ in 0..motion.steps {
-            match motion.direction {
-                Direction::Left =>  head.x -= 1,
-                Direction::Right => head.x += 1,
-                Direction::Up => head.y += 1,
-                Direction::Down => head.y -= 1,
-                Direction::None => { } 
-            };
-
-            if head.x == tail.x && head.y == tail.y {
-                continue;
-            }
-
-            if head.x - tail.x > 1 {
-                tail.x += 1;
-                check_y_distance(&mut head, &mut tail);
-            }
-            else if head.x - tail.x < -1 {
-                tail.x -= 1;
-                check_y_distance(&mut head, &mut tail);
-            }
-
-            if head.y - tail.y > 1 {
-                tail.y += 1;
-                check_x_distance(&mut head, &mut tail);
-            }
-            else if head.y - tail.y < -1 {
-                tail.y -= 1;
-                check_x_distance(&mut head, &mut tail);
-            }
-
-            expand_tail_pos(&mut all_tail_pos, &tail);
-        }
-    }
-
-    return all_tail_pos.len();
-}
-
-fn part2() -> usize {
-    let lines = read_file_lines();
-    let mut rope =  vec![Point { x: 0, y: 0 }; 10];
+    let mut rope =  vec![Point { x: 0, y: 0 }; rope_length];
     let mut all_tail_pos: Vec<Point> = Vec::new();
 
     for line in lines {
@@ -140,18 +93,6 @@ fn part2() -> usize {
 fn expand_tail_pos(tail_pos_vec: &mut Vec<Point>, tail: &Point) {
     if !tail_pos_vec.contains(tail) {
         tail_pos_vec.push(tail.clone());
-    }
-}
-
-fn check_x_distance(head: &mut Point, tail: &mut Point) {
-    if (head.x - tail.x).abs() == 1 {
-        tail.x += head.x - tail.x;
-    }
-}
-
-fn check_y_distance(head: &mut Point, tail: &mut Point) {
-    if (head.y - tail.y).abs() == 1 {
-        tail.y += head.y - tail.y;
     }
 }
 
